f6c8438c215d81abb680c29807c957efcffa882c
Dan Murphy <murphy.dan.s@gmail.com>
2020-11-06 16:26:54 -0500

    Changed State to Objects

--- src/ApCodeInput.js
+++ src/ApCodeInput.js
@@ -3,20 +3,20 @@ import React from 'react';
 class ApCodeInput extends React.Component {
   render() {
 
-
     if(this.props.activeLetter === this.props.thisLetter){
       return (
         <div class="codeEntry" id="activeLetter" ref="titleInput">
           <form >
             <textarea 
-            onChange={this.props.saveAnswer} maxLength="1" autoFocus class="letterEntry" />
+            onChange={this.props.saveAnswer} maxLength="1" autoFocus class="letterEntry" 
+            value = {this.props.currentLetter} />
           </form>
         </div>
       )} else {
         return(
         <div class="codeEntryDisabled" >
           <form >
-            <textarea disabled/>
+            <textarea disabled value = {this.props.currentLetter}/>
           </form>
         </div>
       )}
--- src/App.js
+++ src/App.js
@@ -21,28 +21,25 @@ for(var i = 0; i<10; i++){
   currentGame.push(data[dataPositionsToInclude[i]])
 }
 
-console.log(currentGame)
+  function GameStage(props) {
+    const gameStatus = props.gameStatus;
+    if (gameStatus === 'SelectPool') {
+      return <GameControl updatePoolProp={props.updatePoolProp}
+                          updateStageProp={props.updateStage} />;
+    }
 
+    const letterInput = props.letterInput;
+    return (
+      letterInput.map((letter, key) =>
+        <ApCodeInput activeLetter={props.activeLetter} thisLetter={letter.letterNum}
+                    saveAnswer = {props.saveAnswer} 
+                    currentLetter = {letter.currentLetter} />
+      )
+    )}
 
-
-
-function GameStage(props) {
-  const gameStatus = props.gameStatus;
-  if (gameStatus === 'SelectPool') {
-    return <GameControl updatePoolProp={props.updatePoolProp}
-                        updateStageProp={props.updateStage} />;
-  }
-  return (<div class="codeInput">
-            <ApCodeInput activeLetter={props.activeLetter} thisLetter={1} 
-                         saveAnswer = {props.saveAnswer}/>
-            <ApCodeInput activeLetter={props.activeLetter} thisLetter={2} saveAnswer = {props.saveAnswer}
-            />
-            <ApCodeInput activeLetter={props.activeLetter} thisLetter={3} saveAnswer = {props.saveAnswer}
-             />
-          </div>
-  )
-}
-
+const blankForm = [{letterNum: 0, currentLetter: ""},
+                   {letterNum: 1, currentLetter: ""},
+                   {letterNum: 2, currentLetter: ""}]
 
 class App extends React.Component {
   constructor(props) {
@@ -51,17 +48,18 @@ class App extends React.Component {
       gameStatus: 'SelectPool', 
       gamePool: 'USTop50',
       questionNum: 0,
-      letterEntry: 1,
+      letterEntry: 0,
       currentGame: currentGame,
       currentAnswers: [],
-      currentAnswer: "",
-      currentLetters: ["","",""]
+      letterInput: blankForm,
+      currentAnswer: ""
     }; 
     this.updatePool = this.updatePool.bind(this);
     this.logState = this.logState.bind(this);
     this.saveAnswer = this.saveAnswer.bind(this);
-//    this.letterChange = this.letterChange.bind(this);
   }
+
+  
 
   updatePool(event) {
     this.setState({gamePool: event.target.value, 
@@ -70,55 +68,29 @@ class App extends React.Component {
 
   saveAnswer(event) {
 
-    if(this.state.letterEntry === 1 ) {
+    if(this.state.letterEntry <2 ) {
       
-      const letterFormCtrl = this.state.currentLetters.map((item, index) => 
-                                                      {if(index===0){
-                                                        return(event.target.value)
+      const letterFormCtrl = this.state.letterInput.map((item, index) => 
+                                                      {if(index===this.state.letterEntry){
+                                                        return({letterNum: index, currentLetter: event.target.value})
                                                       } else {
                                                         return(item)
                                                       }
-                                                    }
-                                                      )
- 
-      console.log(letterFormCtrl)
-
+                                                    })                                
       this.setState({
-        currentAnswer:  event.target.value.toUpperCase(), 
         letterEntry: this.state.letterEntry + 1,
-        currentLetters: letterFormCtrl
-        
-      }) 
-    } else if(this.state.letterEntry === 2) {
-      console.log(this.state.currentLetters)
-      const letterFormCtrl = this.state.currentLetters.map((item, index) => 
-                                                      {if(index===1){
-                                                        return(event.target.value)
-                                                      } else {
-                                                        return(item)
-                                                      }}
-                                                      )
-      console.log(letterFormCtrl)
-      this.setState({
-        currentAnswer: this.state.currentAnswer + event.target.value.toUpperCase(), 
-        letterEntry: this.state.letterEntry + 1,
-        currentLetters: letterFormCtrl
-//        currentLetters: [...this.state.currentLetters, event.target.value.toUpperCase()]
+        letterInput: letterFormCtrl,
       }) 
     } else {
-      this.setState((state) => {
-        const newAnswerArr = [...this.state.currentAnswers, this.state.currentAnswer + event.target.value.toUpperCase()]; 
-        return {
-          letterEntry:  1,
-          currentAnswer: "", // Not necessary, but nice
-          currentAnswers: newAnswerArr, 
-          questionNum: this.state.questionNum + 1, 
-          currentLetters: ["", "", ""]
-        }
-    });
+      var ansArr = this.state.letterInput.map((item, index) => item.currentLetter)
+      var threeLetterAnswer = ansArr[0] + ansArr[1] + event.target.value.toUpperCase()
+      this.setState({
+        letterEntry: 0,
+        letterInput: blankForm,
+        currentAnswers: [...this.state.currentAnswers, threeLetterAnswer]        
+      }) 
+    }      
   }
-}
-
 
   logState(){
     console.log(this.state); 
@@ -129,11 +101,13 @@ class App extends React.Component {
     return(
     <div className="App">
       <h1>Do you know your Airport Codes?</h1>
-      <GameStage gameStatus={this.state.gameStatus} 
-        updatePoolProp = {this.updatePool} 
-        activeLetter = {this.state.letterEntry} 
-        saveAnswer = {this.saveAnswer} 
-        currentLetters = {this.currentLetters} />
+      <div class="codeInput">
+        <GameStage gameStatus={this.state.gameStatus} 
+          updatePoolProp = {this.updatePool} 
+          activeLetter = {this.state.letterEntry} 
+          saveAnswer = {this.saveAnswer} 
+          letterInput = {this.state.letterInput} />
+      </div>
       <br/>
       <br/>
       <div onClick={this.logState}>Click to Log State</div>  
